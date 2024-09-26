1d2c2e0cc4b443e0dd5ca86b7f10764c5c86c71b
Gunseli Unsal <gunseliunsal@gmail.com>
2024-09-26 21:15:06 +0300

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Günseli Ünsal on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
